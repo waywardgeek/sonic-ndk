e2660748e2d54f03aa17920916da176516bb4c5b
Bill Cox <waywardgeek@gmail.com>
2011-03-04 12:45:19 -0500

    Upgraded interface to allow users to play with speed, pitch, and rate.

--- src/org/vinuxproject/sonic/SonicTest.java
+++ src/org/vinuxproject/sonic/SonicTest.java
@@ -9,8 +9,11 @@ package org.vinuxproject.sonic;
 
 import java.io.IOException;
 import java.io.InputStream;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SonicTest extends Activity
 {
@@ -18,14 +21,23 @@ public class SonicTest extends Activity
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);                      
- 
-        new Thread( new Runnable() 
+        setContentView(R.layout.main);
+    }
+    
+    public void play(View view)
+    {
+        new Thread(new Runnable() 
         {
             public void run()
-            {        		
+            {
+            	final EditText speedEdit = (EditText) findViewById(R.id.speed);
+            	final EditText pitchEdit = (EditText) findViewById(R.id.pitch);
+            	final EditText rateEdit = (EditText) findViewById(R.id.rate);
+            	float speed = Float.parseFloat(speedEdit.getText().toString());
+            	float pitch = Float.parseFloat(pitchEdit.getText().toString());
+            	float rate = Float.parseFloat(rateEdit.getText().toString());
                 AndroidAudioDevice device = new AndroidAudioDevice(22050, 1);
                 Sonic sonic = new Sonic(22050, 1);
-                float speed = 2.0f; // The amount for sonic to speed up audio
                 byte samples[] = new byte[4096];
                 byte modifiedSamples[] = new byte[2048];
                 InputStream soundFile = getResources().openRawResource(R.raw.talking);
@@ -33,12 +45,11 @@ public class SonicTest extends Activity
 
 				if(soundFile != null) {
 				    sonic.setSpeed(speed);
+				    sonic.setPitch(pitch);
+				    sonic.setRate(rate);
 				    do {
 				        try {
 							bytesRead = soundFile.read(samples, 0, samples.length);
-							if((bytesRead & 1) != 0) {
-							    finish();
-							}
 						} catch (IOException e) {
 							e.printStackTrace();
 							return;
@@ -58,7 +69,6 @@ public class SonicTest extends Activity
 			        	}
 				    } while(bytesRead > 0);
 				    device.flush();
-				    finish();
 				}
             }
         } ).start();
